91d52c58404d641197193acbd331eeb67d080e00
Kovalenko Pavel <kovalenkopasha1997@gmail.com>
2019-11-13 18:03:06 +0200

    Add menu mob and desc

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,48 @@
 $(document).ready(function() {
   $('.search').click(function () {
-    $('.search__inp').slideToggle(function () {
-          $(this).focus();
-    })
+    if ($(window).width() < 1161) {
+      $('.search__inp').toggleClass('open').focus();
+    }
+    else {
+      $('.search__inp').slideToggle(function () {
+        $(this).focus();
+      })
+    }
   });
-  $(function(){
-    $(document).click(function(event) {
-      if ($(event.target).closest(".subheader").length) return;
-      $(".search__inp" ).slideUp();
+  $('.burger').click(function () {
+    $('.subheader').slideToggle(function () {
+      if ($(this).is(':hidden')){
+        $('.search__inp').removeClass('open');
+      }
     });
+    $(this).toggleClass('active');
   });
-  
+  // $(function(){
+  //   $(document).click(function(event) {
+  //     if ($(event.target).closest(".subheader",".header").length) return;
+  //     $(".search__inp" ).slideUp();
+  //   });
+  // });
+  // $('.search__inp').on('submit', function(){
+  //   let value = $(".search__inp").val();
+  //   if (value.length>0){
+  //     return true
+  //   }
+  //   return false;
+  // });
+
+  if ( $(window).width() < 1161) {
+    const slider = document.querySelector('.calculator__menu');
+    slider.remove();
+    document.querySelector('.subheader>.container').appendChild(slider);
+    const search = document.querySelector('.search');
+    search.remove();
+    document.querySelector('.header>.container').appendChild(search);
+    const searchInp = document.querySelector('.search__inp');
+    searchInp.remove();
+    document.querySelector('.header>.container').appendChild(searchInp);
+  }
+
   // popup();
   // slick();
   // burger();
