08d4a66af4a89c0eb3179f38d36c9bdc56384eae
Kovalenko Pavel <kovalenkopasha1997@gmail.com>
2019-11-13 13:04:54 +0200

    Add search anim

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,18 @@
-$(function() {
-  $('#preloader').fadeOut('slow', function() {
-    $(this).remove();
+$(document).ready(function() {
+  $('.search').click(function () {
+    $('.search__inp').slideToggle(function () {
+          $(this).focus();
+    })
   });
-
+  $(function(){
+    $(document).click(function(event) {
+      if ($(event.target).closest(".subheader").length) return;
+      $(".search__inp" ).slideUp();
+    });
+  });
+  
   // popup();
-  slick();
-  burger();
-  search();
-
+  // slick();
+  // burger();
+  // search();
 });
