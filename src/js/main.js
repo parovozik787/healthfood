38bf75b3fa2d89dca72a95ee6d3c9f92cd994319
Kovalenko Pavel <kovalenkopasha1997@gmail.com>
2019-11-20 18:59:06 +0200

    Add news-page

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+  document.querySelector('.filter').addEventListener('click', () => filter.classList.toggle('active'));
+  const filterParams = [...document.querySelectorAll('.filter > .filter__item')];
+  const filter = document.querySelector('.filter');
+  filterParams.forEach(el => {
+    el.addEventListener('click', function () {
+      if (filter.classList.contains('active') && !el.classList.contains(('filter__item--active'))) {
+        document.querySelector('.filter__item--active').classList.remove('filter__item--active');
+        el.classList.add('filter__item--active');
+      }
+    });
+  });
   burger();
   search();
   slick();
@@ -13,4 +24,5 @@ $(document).ready(function() {
     searchInp.remove();
     document.querySelector('.header>.container').appendChild(searchInp);
   }
+
 });
