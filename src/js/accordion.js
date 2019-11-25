5796157020ef64ecc98c6497499b154d6d0f1542
Kovalenko Pavel <kovalenkopasha1997@gmail.com>
2019-11-25 17:18:24 +0200

    Add first edits

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -5,4 +5,18 @@ function accordion() {
             $(this).toggleClass('active');
         })
     }
+    $('.accordion__item ').click(function(e) {
+        e.preventDefault();
+        let $this = $(this);
+        console.log( $this.parent().find('li .accordion__subBox'));
+        if ($this.next('.accordion__subBox').hasClass('show')) {
+            $this.next('.accordion__subBox').removeClass('show');
+            $this.next('.accordion__subBox').slideUp(350);
+        } else {
+            $this.parent().parent().find('li .accordion__subBox').removeClass('show');
+            $this.parent().parent().find('li .accordion__subBox').slideUp(350);
+            $this.next('.accordion__subBox').addClass('show');
+            $this.next('.accordion__subBox').slideDown(350);
+        }
+    });
 }
